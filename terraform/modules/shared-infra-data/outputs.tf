f6dd35a07d2df36468af4d4ebef00c20490f6db5
Ashley Hollis <6480970+AshleyHollis@users.noreply.github.com>
2026-03-01 12:41:43 +1000

    refactor(shared-infra-data): update outputs to use data source attributes

--- terraform/modules/shared-infra-data/outputs.tf
+++ terraform/modules/shared-infra-data/outputs.tf
@@ -1,54 +1,29 @@
 output "resource_group_name" {
   description = "Name of the shared resource group"
-  value       = data.terraform_remote_state.shared_infra.outputs.resource_group_name
+  value       = data.azurerm_resource_group.shared.name
 }
 
 output "resource_group_id" {
   description = "ID of the shared resource group"
-  value       = data.terraform_remote_state.shared_infra.outputs.resource_group_id
+  value       = data.azurerm_resource_group.shared.id
 }
 
 output "resource_group_location" {
   description = "Location of the shared resource group"
-  value       = data.terraform_remote_state.shared_infra.outputs.resource_group_location
+  value       = data.azurerm_resource_group.shared.location
 }
 
 output "key_vault_name" {
   description = "Name of the shared Key Vault"
-  value       = data.terraform_remote_state.shared_infra.outputs.key_vault_name
+  value       = data.azurerm_key_vault.shared.name
 }
 
 output "key_vault_id" {
   description = "ID of the shared Key Vault"
-  value       = data.terraform_remote_state.shared_infra.outputs.key_vault_id
+  value       = data.azurerm_key_vault.shared.id
 }
 
 output "key_vault_uri" {
   description = "URI of the shared Key Vault"
-  value       = data.terraform_remote_state.shared_infra.outputs.key_vault_uri
-}
-
-output "acr_name" {
-  description = "Name of the Azure Container Registry"
-  value       = data.terraform_remote_state.shared_infra.outputs.acr_name
-}
-
-output "acr_login_server" {
-  description = "Login server URL for the Azure Container Registry"
-  value       = data.terraform_remote_state.shared_infra.outputs.acr_login_server
-}
-
-output "acr_id" {
-  description = "ID of the Azure Container Registry"
-  value       = data.terraform_remote_state.shared_infra.outputs.acr_id
-}
-
-output "aks_cluster_name" {
-  description = "Name of the AKS cluster"
-  value       = data.terraform_remote_state.shared_infra.outputs.aks_cluster_name
-}
-
-output "workload_identity_client_id" {
-  description = "Client ID for External Secrets Workload Identity"
-  value       = data.terraform_remote_state.shared_infra.outputs.workload_identity_client_id
+  value       = data.azurerm_key_vault.shared.vault_uri
 }
